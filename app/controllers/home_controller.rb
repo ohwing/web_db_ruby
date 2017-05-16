9c5386b04890f837ac3c685adcaf30eaa64fce84
ohjeung <ohjeung@likelion.org>
2017-05-16 13:05:22 +0900

    최종완료

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def write
+    @pond = Post.new
+    @pond.name = params[:input_name]
+    @pond.content = params[:input_content]
+    @pond.save
+    redirect_to '/'
+  end
 end
